c87c184453119ef7a51c942101c1c3e16e9fc9e7
natsuki <ashi2727n@gmail.com>
2022-11-25 13:45:48 +0900

    create mongo-go

---
+++ Assifnment4/mongo-go/src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	log.Println(os.Getenv("MONGO_URI"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connection to MongoDB")
+}
+
+func main() {
+	log.Println("Go Run")
+}
